86ce07a05b7f49ab1c467ea5db3e635142fa53c5
ChiragGoyal2222 <chirag.goyal@students.iiit.ac.in>
2025-01-01 03:57:22 +0530

    deployed after fixing.

--- experiment/simulation/js/check.js
+++ experiment/simulation/js/check.js
@@ -1,7 +1,3 @@
-"use strict";
-import * as THREE from "https://threejsfundamentals.org/threejs/resources/threejs/r115/build/three.module.js";
-
-const origin = { x: 0, y: 0, z: 0 };
 export const createArm = function (
   scene,
   hand_comp,
@@ -12,7 +8,6 @@ export const createArm = function (
   palm_dim,
   palm_pos
 ) {
-  // Creating groups, which hold the arm, forearm and palm
   const shoulder = new THREE.Group();
   shoulder.position.set(origin.x, origin.y, origin.z);
 
@@ -30,7 +25,7 @@ export const createArm = function (
   const arm_box_geo = new THREE.BoxGeometry(arm_dim.x, arm_dim.y, arm_dim.z);
   const arm_box_mat = new THREE.MeshBasicMaterial({ color: 0xff7f50 });
   const arm_box = new THREE.Mesh(arm_box_geo, arm_box_mat);
-  arm_box.position.set(arm_dim.x / 2, -arm_dim.y / 2, 0);
+  arm_box.position.set(arm_dim.x / 2, -arm_dim.y / 1.35, 0);
 
   const arm = new THREE.Group();
   arm.add(arm_cyl);
@@ -45,38 +40,38 @@ export const createArm = function (
   const elbow_cyl_mat = new THREE.MeshBasicMaterial({ color: 0xff69b4 });
   const elbow_cyl = new THREE.Mesh(elbow_cyl_geo, elbow_cyl_mat);
   elbow_cyl.rotation.x = Math.PI / 2;
-  elbow_cyl.position.set(0, -fore_dim.y / 2, 0);
+  elbow_cyl.position.set(0, -1.5, 0);
 
-  const f_geo = new THREE.BoxGeometry(fore_dim.x, fore_dim.y, fore_dim.z);
+  const f_geo = new THREE.BoxGeometry(2 * fore_dim.x, fore_dim.y, fore_dim.z);
   const f_mat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   const fore_arm = new THREE.Mesh(f_geo, f_mat);
-  fore_arm.position.set(fore_dim.x / 2, fore_dim.y / 2, 0);
+  fore_arm.position.set(fore_dim.x, -1.5, 0);
 
   const elbow = new THREE.Group();
   elbow.add(elbow_cyl);
   elbow.add(fore_arm);
   elbow.position.set(arm_dim.x, -arm_dim.y, 0);
-  const wrist = new THREE.Group();
-  wrist.position.set(fore_dim.x, 0, 0);
 
-    const wrist_cyl_geo = new THREE.CylinderGeometry(
-        arm_dim.y / 4,
-        arm_dim.y / 4,
-        arm_dim.z,
-        32
-    );
-const wrist_cyl_mat = new THREE.MeshBasicMaterial({ color: 0xa52a2a });
-    const wrist_cyl = new THREE.Mesh(wrist_cyl_geo, wrist_cyl_mat);
-    wrist_cyl.rotation.x = Math.PI / 2;
-    
+  const wrist_cyl_geo = new THREE.CylinderGeometry(
+    arm_dim.y / 4,
+    arm_dim.y / 4,
+    arm_dim.z,
+    32
+  );
+  const wrist_cyl_mat = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const wrist_cyl = new THREE.Mesh(wrist_cyl_geo, wrist_cyl_mat);
+  wrist_cyl.rotation.x = Math.PI / 2;
+  wrist_cyl.position.set(2 * fore_dim.x, 0, 0);
+
   const p_geo = new THREE.BoxGeometry(palm_dim.x, palm_dim.y, palm_dim.z);
   const p_mat = new THREE.MeshBasicMaterial({ color: 0x00abcd });
   const palm = new THREE.Mesh(p_geo, p_mat);
-  palm.position.set(palm_dim.x / 2, 0, 0);
+  palm.position.set(2 * palm_dim.x + palm_dim.x / 2, 0, 0);
 
-    // adding objects in a hierarchical fashion
+  const wrist = new THREE.Group();
   wrist.add(wrist_cyl);
   wrist.add(palm);
+  wrist.position.set(0, -1.5, 0);
 
   elbow.add(fore_arm);
   elbow.add(wrist);
@@ -90,15 +85,4 @@ const wrist_cyl_mat = new THREE.MeshBasicMaterial({ color: 0xa52a2a });
   hand_comp.push(shoulder);
   hand_comp.push(elbow);
   hand_comp.push(wrist);
-};
-
-export const moveArm = function (hand_comp, moveBy) {
-  // hand_comp.forEach(Object => {
-  // let curPos = new Vector3( Object.position.x, Object.position.y, Object.position.z );
-  // let newPos = new THREE.Vector3( Object.position.x + moveBy.x, Object.position.y + moveBy.y, Object.position.z + moveBy.z);
-
-  hand_comp[0].translateOnAxis(new THREE.Vector3(1, 0, 0), moveBy.x);
-  hand_comp[0].translateOnAxis(new THREE.Vector3(0, 1, 0), moveBy.y);
-  hand_comp[0].translateOnAxis(new THREE.Vector3(0, 0, 1), moveBy.z);
-  // });
 };
